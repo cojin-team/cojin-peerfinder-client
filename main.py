ca27ba7967c4e4f9de6ce31e321e0dcfc76fa8e1
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-06-29 12:43:23 +0200

    delete popups and add better console output

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 from core.configparser import parser
 from core.api import wrapper
 from core.rpc import RPC
+from core.uncolor import uncolor
 
 # External libraries
 from pyngrok import ngrok
-from pymsgbox import alert
 
 # Internal python libraries
 from os import getenv, path
@@ -24,15 +24,18 @@ else:
 
 # Error msg if cojin file doesn't exists
 if not path.isfile(cojinpath):
-    alert('The cojin configuration file does not exists, please create and fill it', 'Cojin Configuration Error')
+    print(uncolor('The cojin configuration file does not exists, please create and fill it', ['red']))
+    input('press enter to exit')
     exit()
 
+print(uncolor('Parsing cojin.conf file', ['yellow']))
 cfg.read(cojinpath)
 
 # Test wallet rpc
 
 if not cfg.rpcOK():
-    alert('The wallet RPC is not configured, please configure it and restart the wallet', 'Wallet RPC Error')
+    print(uncolor('The wallet RPC is not configured, please configure it and restart the wallet', ['red']))
+    input('press enter to exit')
     exit()
 
 try:
@@ -42,29 +45,33 @@ try:
         cfg.rpc.password
     )
 except Exception as e:
-    alert('The wallet RPC refuses the connection, try restarting the wallet', 'Wallet RPC Error')
+    print(uncolor('The wallet RPC refuses the connection, try restarting the wallet', ['red']))
+    input('press enter to exit')
     exit()
 
 # Actual peer finding code
 
+print(uncolor('Trying connection with server', ['yellow']))
 server = wrapper('https://cojin-peerfinder.glitch.me')
+print(uncolor('Connected with server', ['green']))
 
 peers = server.getPeers()
 for peer in peers:
     if peer == None: continue
+    print('Trying connection to:', uncolor(peer, ['green']))
     rpc.request('addnode', [peer, 'onetry']) # use onetry bc instantly attempts the connection
     sleep(1) # avoid accidental DOS attack
 
 
 if True: #replace with programconfig.enablePortFoward (if program config is added)
-    print('Posting peer on peerfinding server')
+    print(uncolor('Posting peer on peerfinding server', ['yellow']))
     tunnel = ngrok.connect(cfg.walletport, 'tcp')
     server.postPeer(tunnel.data['public_url'])
 
     try:
         process = ngrok.get_ngrok_process()
-        print('Peer posted, press ctrol + C to kill ngrok')
+        print(uncolor('Peer posted, press ctrol + C to kill ngrok', ['green']))
         process.proc.wait()
     except KeyboardInterrupt:
-        print('Shutting down')
+        print(uncolor('Shutting down', ['yellow']))
         ngrok.kill()
